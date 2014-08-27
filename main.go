e66e443914833257d2c79b2e870fd64be146f787
dbarbera <diego@dbarbera.name>
2014-08-27 19:22:39 +0200

    Extend CLI support

--- main.go
+++ main.go
@@ -1,69 +1,87 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"os"
-
-	"github.com/peterh/liner"
 )
 
-type Enviroment struct {
-	symbols map[string]Object
-	parent  *Enviroment
+const Version = "0.0.1"
+
+type Options struct {
+	Help    bool
+	Version bool
+	Script  string
+	Args    []string
 }
 
-func NewEnviroment() *Enviroment {
-	return &Enviroment{symbols: make(map[string]Object)}
-}
+func parse(args []string) (*Options, error) {
+	var options Options
 
-func NewChildEnviroment(parent *Enviroment) *Enviroment {
-	return &Enviroment{symbols: make(map[string]Object), parent: parent}
-}
+	for i, arg := range args {
+		if arg == "-h" || arg == "--help" {
+			options.Help = true
+			continue
+		}
 
-func (self *Enviroment) Define(sym *Symbol, obj Object) {
-	self.symbols[sym.Value] = obj
-}
+		if arg == "-v" || arg == "--version" {
+			options.Version = true
+			continue
+		}
 
-func (self *Enviroment) Resolve(sym *Symbol) Object {
-	obj := self.symbols[sym.Value]
-	if obj == nil && self.parent != nil {
-		return self.parent.Resolve(sym)
+		if arg[0] == '-' {
+			return nil, errors.New("Unexpected flag")
+		}
+
+		options.Script = arg
+		options.Args = args[i+1:]
+		break
 	}
-	return obj
+
+	return &options, nil
 }
 
 func main() {
-	os.Exit(repl())
+	options, err := parse(os.Args[1:])
+	if err != nil {
+		os.Exit(usage())
+	}
+
+	if options.Help {
+		os.Exit(usage())
+	}
+
+	if options.Version {
+		os.Exit(version())
+	}
+
+	slip := NewSlip()
+
+	if options.Script != "" {
+		source, err := ioutil.ReadFile(options.Script)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading script: %s\n", err)
+			os.Exit(1)
+		}
+		os.Exit(slip.Run(string(source)))
+	} else {
+		os.Exit(slip.Repl())
+	}
 }
 
-func repl() int {
-	liner := liner.NewLiner()
-	defer liner.Close()
-
-	reader := NewReader()
-	env := NewEnviroment()
-
-	for name, fn := range CoreFuncs {
-		env.Define(NewSymbol(name), NewPrimFunc(fn))
-	}
-
-	for {
-		line, err := liner.Prompt("slip> ")
-
-		if err == io.EOF {
-			fmt.Println()
-			return 0
-		} else if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading line: %s\n", err)
-			return 1
-		}
-
-		reader.Init(line)
-		for obj := reader.Read(); obj != nil; obj = reader.Read() {
-			fmt.Println(obj.Eval(env))
-		}
-	}
-
+func version() int {
+	fmt.Printf("Slip %s\n", Version)
 	return 0
+}
+
+func usage() int {
+	fmt.Fprintln(os.Stderr, "Usage: slip [options] [script [args]]")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "Options:")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "-h, --help       Show this help")
+	fmt.Fprintln(os.Stderr, "-v, --version    Show version information")
+	fmt.Fprintln(os.Stderr, "")
+	return 1
 }
---
+++ slip.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/peterh/liner"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// Slip
+
+type Slip struct {
+	env    *Enviroment
+	reader *Reader
+}
+
+func NewSlip() *Slip {
+	env := NewEnviroment()
+
+	for name, fn := range CoreFuncs {
+		env.Define(NewSymbol(name), NewPrimFunc(fn))
+	}
+
+	return &Slip{env: env, reader: NewReader()}
+}
+
+func (self *Slip) Repl() int {
+	liner := liner.NewLiner()
+	defer liner.Close()
+
+	for {
+		line, err := liner.Prompt("slip> ")
+
+		if err == io.EOF {
+			fmt.Println()
+			return 1
+		} else if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading line: %s\n", err)
+			return 1
+		}
+
+		self.reader.Init(line)
+		for obj := self.reader.Read(); obj != nil; obj = self.reader.Read() {
+			fmt.Println(obj.Eval(self.env))
+		}
+	}
+
+	return 0
+}
+
+func (self *Slip) Run(src string) int {
+	self.reader.Init(src)
+	for obj := self.reader.Read(); obj != nil; obj = self.reader.Read() {
+		obj.Eval(self.env)
+	}
+	return 0
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Enviroment
+
+type Enviroment struct {
+	symbols map[string]Object
+	parent  *Enviroment
+}
+
+func NewEnviroment() *Enviroment {
+	return &Enviroment{symbols: make(map[string]Object)}
+}
+
+func NewChildEnviroment(parent *Enviroment) *Enviroment {
+	return &Enviroment{symbols: make(map[string]Object), parent: parent}
+}
+
+func (self *Enviroment) Define(sym *Symbol, obj Object) {
+	self.symbols[sym.Value] = obj
+}
+
+func (self *Enviroment) Resolve(sym *Symbol) Object {
+	obj := self.symbols[sym.Value]
+	if obj == nil && self.parent != nil {
+		return self.parent.Resolve(sym)
+	}
+	return obj
+}
